a5812b751c3827559eeafee81f73d2a67af6946c
NehalikaVadlamudi <nehalika100@gmail.com>
2024-02-19 19:24:40 -0600

    changed content

---
+++ .scripts/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+echo "Deployment started ..."
+# Pull the latest version of the app
+git fetch origin master
+git reset --hard origin/master
+git pull origin master
+echo "Deployment finished!"
